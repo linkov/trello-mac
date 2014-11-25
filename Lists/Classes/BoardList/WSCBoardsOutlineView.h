2e7a46c9c06f47c897d8dfc8fb89201b2a1ab10c
Alex Linkov <alexey.linkov@gmail.com>
2014-11-25 23:55:04 +0300

    don't duplicate protocol definitions

--- Lists/Classes/BoardList/WSCBoardsOutlineView.h
+++ Lists/Classes/BoardList/WSCBoardsOutlineView.h
@@ -6,15 +6,8 @@
 //  Copyright (c) 2014 SDWR. All rights reserved.
 //
 
-@protocol SDWBoardsListOutlineViewDelegate;
 
 #import <Cocoa/Cocoa.h>
-
-@interface WSCBoardsOutlineView : NSOutlineView
-
-@property (weak) id <SDWBoardsListOutlineViewDelegate> menuDelegate;
-
-@end
 
 @protocol SDWBoardsListOutlineViewDelegate
 
@@ -23,5 +16,11 @@
 - (void)outlineviewShouldDeleteListAtRow:(NSUInteger)listRow;
 - (void)outlineviewShouldAddListBelowRow:(NSUInteger)listRow;
 
+
+@end
+
+@interface WSCBoardsOutlineView : NSOutlineView
+
+@property (weak) id <SDWBoardsListOutlineViewDelegate> menuDelegate;
 
 @end
--- Lists/Classes/CardList/SDWMenuItemImageView.h
+++ Lists/Classes/CardList/SDWMenuItemImageView.h
@@ -5,7 +5,6 @@
 //  Created by alex on 11/7/14.
 //  Copyright (c) 2014 SDWR. All rights reserved.
 //
-@protocol SDWMenuItemDelegate;
 
 typedef enum {
 
@@ -15,17 +14,18 @@ typedef enum {
 
 #import <Cocoa/Cocoa.h>
 
-@interface SDWMenuItemImageView : NSImageView
-
-@property (weak, nonatomic) id <SDWMenuItemDelegate> delegate;
-
-@end
-
 
 @protocol SDWMenuItemDelegate
 
 @optional
 
 - (void)menuItemShouldValidateDropWithAction:(SDWMenuItemDropAction)action objectID:(NSString *)objectID;
+
+@end
+
+
+@interface SDWMenuItemImageView : NSImageView
+
+@property (weak, nonatomic) id <SDWMenuItemDelegate> delegate;
 
 @end
