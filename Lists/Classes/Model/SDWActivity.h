c10510ecd2f9957d9d7770358d8aa4aa75712730
Alex Linkov <alexey.linkov@gmail.com>
2014-12-14 17:01:03 +0300

    add activity model class

---
+++ Lists/Classes/Model/SDWActivity.h
@@ -0,0 +1,13 @@
+//
+//  SDWActivity.h
+//  Lists
+//
+//  Created by alex on 12/14/14.
+//  Copyright (c) 2014 SDWR. All rights reserved.
+//
+
+#import "AFRecordModel.h"
+
+@interface SDWActivity : AFRecordModel
+
+@end
---
+++ Lists/Classes/Model/SDWActivity.m
@@ -0,0 +1,13 @@
+//
+//  SDWActivity.m
+//  Lists
+//
+//  Created by alex on 12/14/14.
+//  Copyright (c) 2014 SDWR. All rights reserved.
+//
+
+#import "SDWActivity.h"
+
+@implementation SDWActivity
+
+@end
